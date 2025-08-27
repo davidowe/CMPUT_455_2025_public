ac75195cdb45a4094ca2cb8a7ebdbf7a12c68df8
davidowe <davidowe@ualberta.ca>
2025-08-26 20:15:50 -0600

    adding simple searches and tictactoe

---
+++ dfs_bfs_examples.py
@@ -0,0 +1,123 @@
+
+def dfs_state(start_state, goal):
+    stack = [start_state]
+    visited = set()
+    while len(stack) > 0:
+        state = stack.pop()
+        if state not in visited:
+            visited.add(state)
+            if goal(state):
+                return state
+            stack += state.children
+    return None
+
+def dfs_recursive(state, visited, goal):
+    if goal(state):
+        return state
+    visited.add(state)
+    for child in state.children:
+        if child not in visited:
+            found_state = dfs_recursive(child, visited, goal)
+            if found_state is not None:
+                return found_state
+    return None
+
+def dfs_general(state, visited, goal):
+    visited.add(state)
+    if goal(visited):
+        return visited
+    for child in state.children:
+        if child not in visited:
+            found_visited = dfs_general(child, visited, goal)
+            if found_visited is not None:
+                return found_visited
+    return None
+
+def dfs_path(path, visited, goal):
+    if goal(path):
+        return path
+    visited.add(tuple(path))
+    for child in path[-1].children:
+        child_path = path + [child]
+        if tuple(child_path) not in visited:
+            found_path = dfs_path(child_path, visited, goal)
+            if found_path is not None:
+                return found_path
+    return None
+
+
+def dfs_path_to_end(path, visited, end):
+    if path[-1] == end:
+        return path
+    visited.add(tuple(path))
+    for child in path[-1].children:
+        child_path = path + [child]
+        if tuple(child_path) not in visited:
+            found_path = dfs_path_to_end(child_path, visited, end)
+            if found_path is not None:
+                return found_path
+    return None
+
+def dfs_heuristic(path, visited, end, heuristic):
+    if path[-1] == end:
+        return path
+    visited.add(tuple(path))
+    ranked_children = sorted(path[-1].children, key=heuristic)
+    for child in ranked_children:
+        child_path = path + [child]
+        if tuple(child_path) not in visited:
+            found_path = dfs_heuristic(child_path, visited, \
+                                        end, heuristic)
+            if found_path is not None:
+                return found_path
+    return None
+
+def bfs_state(start_state, goal):
+    queue = [start_state]
+    visited = set()
+    while len(queue) > 0:
+        state = queue.pop()
+        if state not in visited:
+            if goal(state):
+                return state
+            visited.add(state)
+            queue = state.children + queue
+    return None
+
+def bfs_path(start_state, goal):
+    queue = [start_state]
+    visited = set(queue)
+    while len(queue) > 0:
+        state = queue.pop()
+        if goal(state):
+            return state
+        for child in state.children:
+            if child not in visited:
+                visited.add(child)
+                child.parent = state
+                queue.insert(0, child)
+    return None
+
+def find_recorded_path(state):
+    if state.parent is None:
+        return [state]
+    else:
+        return find_recorded_path(state.parent) + [state]
+
+#path = find_recorded_path(bfs_path(start_state, goal))
+
+def A_star(start_state, end_state, distance, heuristic):
+    prio_queue = [(start_state, 0)]
+    visited = set([start_state])
+    while len(prio_queue) > 0:
+        state, value = prio_queue.pop()
+        if state == end_state:
+            return state
+        for child in state.children:
+            if child not in visited:
+                visited.add(child)
+                child.parent = state
+                prio_queue.append((child, distance(start_state, child) \
+                                    + heuristic(start_state, end_state)))
+        prio_queue.sort(key=lambda x:x[1], reverse=True)
+    return None
---
+++ tictactoe.py
@@ -0,0 +1,74 @@
+class TicTacToe:
+    def __init__(self, init_board = [[0]*3,[0]*3,[0]*3], init_to_play = 1):
+        self.to_play = init_to_play
+        self.board = init_board
+
+    def get_children(self):
+        children = []
+        if not self.is_terminal():
+            for y in range(3):
+                for x in range(3):
+                    if self.board[y][x] == 0:
+                        child_board = [list(self.board[y]) for y in range(3)]
+                        child_board[y][x] = self.to_play
+                        self.children.append(TicTacToe(child_board, 2 if self.to_play == 1 else 1))
+        return children
+    
+    def make_move(self, move):
+        self.board[move[0]][move[1]] = self.to_play
+        self.to_play = 1 if self.to_play == 2 else 2
+
+    def undo_move(self, move):
+        self.board[move[0]][move[1]] = 0
+        self.to_play = 1 if self.to_play == 2 else 2
+
+    def get_moves(self):
+        moves = []
+        for y in range(3):
+            moves += [(x, y) for x in range(3) if self.board[y][x] == 0]
+        return moves
+
+    def player_to_str(self, player):
+        if player == 0:
+            return "_"
+        elif player == 1:
+            return "X"
+        elif player == 2:
+            return "O"
+        else:
+            raise ValueError("Unknown board value")
+    
+    def print_board(self):
+        for y in range(3):
+            for x in range(3):
+                print(self.player_to_str(self.board[y][x]), end=" ")
+            print()
+        print("To play:", self.player_to_str(self.to_play))
+
+    #Returns if the state is terminal, and the winner (None for draw)
+    def is_terminal(self):
+        for piece in [1, 2]:
+            #Horizontal lines
+            for y in range(3):
+                if self.board[y][0] == piece and self.board[y][1] == piece and self.board[y][2] == piece:
+                    return True, piece
+            #Vertical lines
+            for x in range(3):
+                if self.board[0][x] == piece and self.board[1][x] == piece and self.board[2][x] == piece:
+                    return True, piece
+            #Diagonal line
+            if self.board[0][0] == piece and self.board[1][1] == piece and self.board[2][2] == piece:
+                return True, piece
+            #Anti-diagonal line
+            if self.board[0][2] == piece and self.board[1][1] == piece and self.board[2][0] == piece:
+                return True, piece
+        #Check for draw
+        any_empty = False
+        for y in range(3):
+            for x in range(3):
+                if self.board[y][x] == 0:
+                    any_empty = True
+                    break
+            if any_empty:
+                break
+        return not any_empty, None
